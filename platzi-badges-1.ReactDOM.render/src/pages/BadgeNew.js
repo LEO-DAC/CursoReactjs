3f1751febfb0d01cefd015d470962534303ee471
LEO-DAC <leonardodanielalonso1996@gmail.com>
2021-06-14 23:52:30 -0500

    cambios en proyecto

--- platzi-badges-1.ReactDOM.render/src/pages/BadgeNew.js
+++ platzi-badges-1.ReactDOM.render/src/pages/BadgeNew.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
+import header from '../images/badge-header.svg';
 class BadgeNew extends React.Component{
     render(){
         return <div>
                   <Navbar/>
+                  <div className="BadgeNew__hero">
+                      <img className="img-fluid" src={header} alt="Logo"/>
+                  </div>
                </div>;
+
+               <div className="container">
+                     <div className="row">
+                          <div className="col">
+                              </div>              
+                     </div>
+
+               </div>
     }
 }
+
+export default BadgeNew;
